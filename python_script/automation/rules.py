66072523551587eed3d0c1a9e8159d4082cf0141
Asim Maroof <aasm02703@gmail.com>
2025-06-15 19:01:05 +0800

    making changes to logstash

--- python_script/automation/rules.py
+++ python_script/automation/rules.py
@@ -3,12 +3,15 @@ import json
 
 def extract_paranoia_rules(input_text):
     # Normalize line breaks and combine multiline SecRule blocks
-    lines = input_text.splitlines()
+    lines = input_text.splitlines() # return lists
     rules = []
     buffer = []
     
     for line in lines:
         line = line.strip()
+        if line.startswith("#") or not line:
+            continue  # Skip comments and empty lines
+        # if line
         if line.startswith("SecRule"):
             if buffer:
                 rules.append("\n".join(buffer))
@@ -29,13 +32,22 @@ def extract_paranoia_rules(input_text):
 
     return result
 
+
+
+
 # Example usage
 if __name__ == "__main__":
+    matched_id = "942251"
     with open("REQUEST-942-APPLICATION-ATTACK-SQLI.conf", "r") as file:
         input_text = file.read()
 
     extracted_rules = extract_paranoia_rules(input_text)
 
+    with open("custom_rules.conf", "a") as output_file:
+        if matched_id in extracted_rules:
+            output_file.write(extracted_rules[matched_id] + "\n\n")
+        else:
+            print(f"Rule with ID {matched_id} not found in extracted rules.")
+    
     # Output as pretty JSON
     print(json.dumps(extracted_rules, indent=2))
-
